109a173876ff9bd3ad79acbeff264b646b2499af
MarcoAgostinoMelo <marco.agostinoo@hotmail.com>
2025-02-12 03:10:00 -0300

    jdklasdioqwueqowiue

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,15 +6,23 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'http://localhost:3000'],
+          'connect-src': ["'self'", 'http://localhost:3000', 'https://api.example.com'],
+          'img-src': ["'self'", 'data:', 'https://*.example.com'],
+          'script-src': ["'self'", "'unsafe-inline'"],
         },
       },
     },
   },
   'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://your-frontend-domain.com', 'http://localhost:3000'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
