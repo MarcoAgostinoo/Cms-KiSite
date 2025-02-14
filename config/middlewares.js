ee082fe4403fc529dbf00efc09ca039b6aa236ab
MarcoAgostinoMelo <marco.agostinoo@hotmail.com>
2025-02-13 21:39:56 -0300

    imagemprofile

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,23 +6,15 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'http://localhost:3000', 'https://api.example.com'],
-          'img-src': ["'self'", 'data:', 'https://*.example.com'],
-          'script-src': ["'self'", "'unsafe-inline'"],
+          'connect-src': ["'self'", 'http://localhost:3000'],
         },
       },
     },
   },
   'strapi::logger',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['https://your-frontend-domain.com', 'http://localhost:3000'],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization'],
-      credentials: true,
-    },
-  },
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://cms-kisite-production.up.railway.app'),
   app: {
     keys: env.array('APP_KEYS'),
   },
